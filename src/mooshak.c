9dd1a91d0183b67e1859495b90bb0e07352f2b7f
Grabiel14 <33735410+Grabiel14@users.noreply.github.com>
2018-03-18 11:00:54 +0000

    New file which contains the solution to be submitted to Mooshak
    This solution uses the Tarjan algorithm and adjacency lists
    
    	new file:   src/mooshak.c

---
+++ src/mooshak.c
@@ -0,0 +1,330 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+typedef struct node {
+  struct node * next;
+  int id;
+} * Node;
+
+
+typedef struct graph {
+  Node * adjList;
+  int n_vertexes;
+  int n_connections;
+} * Graph;
+
+
+
+
+static Node * adjListAuxPointer;
+static int * translation;
+static int n_connections;
+
+static void freeNode(int from, Node to);
+static void traverseGraph(Graph g, void (*func)(int, Node));
+static void invertConnection(int from, Node to);
+static void insertInAdjList(Node * adjList, int id);
+static void reduceGraph_aux(Graph g, int u, int v);
+static void printSccGraph_aux();
+
+int nVertex(Graph g) { return g->n_vertexes; }
+int nConnection(Graph g) { return g->n_connections; }
+
+
+/* frees memory associated with a node*/
+void freeNode(int i, Node n) {
+  (void) i;
+  free(n);
+}
+
+
+/* inserts a new node at the beggining of the adjacency list */
+void insertInAdjList(Node * adjList, int id) {
+
+  Node new = (Node) calloc(1, sizeof(struct node));
+  new->id = id;
+  new->next = *adjList;
+  *adjList = new;
+}
+
+int insertOrderlyInAdjList(Node * adjList, int id) {
+
+  Node new = (Node) calloc(1, sizeof(struct node));
+  new->id = id;
+  Node conn = *adjList;
+
+  if(conn == NULL)
+    *adjList = new;
+
+  else if(conn->id > id) {
+    *adjList = new;
+    new->next = conn;
+  }
+
+  else {
+
+    while(conn->next != NULL && conn->next->id < id)
+      conn = conn->next;
+    if(conn->id == id || (conn->next != NULL && conn->next->id == id))
+      return 0;
+    new->next = conn->next;
+    conn->next = new;
+
+  }
+
+  return 1;
+
+}
+
+
+
+/* function which receives input and builds the adjacency list*/
+Graph buildGraph() {
+  int N, M, vertex, edge;
+  scanf("%d", &N);
+  scanf("%d", &M);
+
+  Graph res = (Graph) calloc(1, sizeof(struct graph));
+  res->adjList = (Node*) calloc(1, sizeof(Node) * N);
+
+  res->adjList--; /*the vertexes are bounded from 1 to N*/
+
+  res->n_vertexes = N;
+
+  res->n_connections = M;
+
+  while(M--) {
+    scanf("%d %d", &vertex, &edge);
+    insertInAdjList(res->adjList + vertex, edge);
+  }
+
+  return res;
+}
+
+
+/* Meta function to do something with the nodes in the adjacency list
+ * It goes in order of vertexes */
+void traverseGraph(Graph g, void (*func)(int, Node)){
+  int i;
+  Node conn, scratchpad;
+  for(i=1; i <= g->n_vertexes; ++i) {
+    conn = g->adjList[i];
+    while(conn != NULL) {
+      scratchpad = conn;
+      conn = conn->next;
+      func(i, scratchpad);
+    }
+  }
+}
+
+/* Insert (inverted) connection in new adjacency list auxiliary pointer */
+void invertConnection(int from, Node to) {
+  insertInAdjList(adjListAuxPointer + to->id, from);
+}
+
+
+/* Transposes list of adjacencies, one node at a time */
+Graph transposeGraph(const Graph g) {
+
+  adjListAuxPointer = (Node*) calloc(1, sizeof(Node) * g->n_vertexes);
+  adjListAuxPointer--;
+  traverseGraph(g, invertConnection);
+  Graph res = (Graph) calloc(1, sizeof(struct graph));
+  res->n_vertexes = g->n_vertexes;
+  res->n_connections = g->n_connections;
+  res->adjList = adjListAuxPointer;
+  return res;
+
+}
+
+
+/* Shows that everything went fine (for debugging purposes)*/
+void showGraph(const Graph g) {
+  int i;
+  Node conn;
+  for(i=1; i <= g->n_vertexes; ++i) {
+    printf("Vertex %d: ", i);
+    conn = g->adjList[i];
+    while(conn != NULL) {
+      printf("%d ", conn->id);
+      conn = conn->next;
+    }
+    putchar('\n');
+  }
+}
+
+
+
+void freeGraph(Graph g) {
+  traverseGraph(g, freeNode);
+  free(++(g->adjList));
+  free(g);
+}
+
+void doForEachAdjU(Graph g, int u, void (*func)(Graph, int, int)) {
+  Node conn, scratchpad;
+  conn = g->adjList[u];
+  while(conn != NULL) {
+    scratchpad = conn;
+    conn = conn->next;
+    func(g, u, scratchpad->id);
+  }
+}
+
+
+
+Graph reduceGraph(Graph g, int * trans) {
+
+  int i;
+  translation = trans;
+
+  Graph res = (Graph) calloc(1, sizeof(struct graph));
+  res->adjList = adjListAuxPointer = (Node*) calloc(1, sizeof(Node) * nVertex(g));
+
+  res->adjList--; /*the vertexes are bounded from 1 to N*/
+  adjListAuxPointer--;
+
+  res->n_vertexes = nVertex(g);
+
+  for(i = 1; i <= nVertex(g); i++)
+    doForEachAdjU(g, i, reduceGraph_aux);
+
+  res->n_connections = n_connections;
+
+  return res;
+}
+
+
+void reduceGraph_aux(Graph g, int u, int v) {
+
+  (void) g;
+  v = translation[v];
+  u = translation[u];
+
+  if(u != v) {
+    n_connections += insertOrderlyInAdjList(adjListAuxPointer + u, v);
+  }
+}
+
+
+
+void printSccGraph(Graph g, int nScc) {
+
+  printf("%d\n%d\n", nScc, g->n_connections);
+
+  int i;
+  for(i = 1; i <= nVertex(g); i++)
+    doForEachAdjU(g, i, printSccGraph_aux);
+}
+
+void printSccGraph_aux(Graph g, int vertex, int conn) {
+
+  (void) g;
+  printf("%d %d\n", vertex, conn);
+
+}
+
+
+
+
+
+static int visited = 0;
+static int n_scc = 0;
+/* Points to the first element that exists */
+static int stackPointer = -1;
+static int * discovery;
+static int * low;
+static int * translation;
+static int * scratch; static int scratchPointer=0;
+static int * stack;
+static int * in_stack;
+
+static void Tarjan(Graph g);
+static void tarjanVisit(Graph g, int vertex);
+
+
+#define min(a,b) (a<b?a:b)
+
+#define stack_contains(a) (in_stack[a])
+
+void Tarjan(Graph g) {
+  /* because we really want to go [1, nVertex], we do [0, nVertex] */
+  int V = nVertex(g) + 1;
+  int E = nConnection(g);
+  /* the stack can't go over the number of connections in the graph */
+  stack = (int*) calloc(E, sizeof(int));
+  in_stack = (int*) calloc(V, sizeof(int));
+  scratch = (int*) calloc(E, sizeof(int));
+  translation = (int*) calloc(V, sizeof(int));
+  discovery = (int*) malloc(sizeof(int)*V);
+  low = (int*) malloc(sizeof(int)*V);
+
+  int i;
+  for(i=1; i<V; i++)
+    low[i] = INT_MAX;
+  for(i=1; i<V; i++)
+    discovery[i] = INT_MAX;
+
+  for(i = 1; i < V; i++)
+    if (low[i] == INT_MAX)
+      tarjanVisit(g, i);
+
+}
+
+
+void tarjanVisit_aux(Graph g, int from, int to) {
+
+  if (low[to] == INT_MAX || stack_contains(to)) {
+      if(low[to] == INT_MAX) {
+        tarjanVisit(g, to);
+      }
+      low[from]=min(low[from],low[to]);
+  }
+}
+
+void tarjanVisit(Graph g, int vertex) {
+
+  int v;
+  discovery[vertex] = low[vertex] = visited++;
+  stack[++stackPointer] = vertex; /*Push*/
+  in_stack[vertex] = 1;
+
+  doForEachAdjU(g, vertex, tarjanVisit_aux);
+
+  if(discovery[vertex] == low[vertex]) {
+    int lowest_in_scc = vertex;
+    n_scc++;
+    while(vertex != (v = stack[stackPointer--])) { /*Pop*/
+      in_stack[v] = 0;
+      lowest_in_scc = min(v,lowest_in_scc);
+      scratch[++scratchPointer] = v;
+    }
+    scratch[++scratchPointer] = vertex;
+    in_stack[vertex] = 0;
+    while(0 != (v=scratch[scratchPointer--])) {
+      translation[v] = lowest_in_scc;
+    }
+    scratchPointer++; /*temporary solution*/
+  }
+}
+
+
+
+
+int main() {
+  Graph ola = buildGraph();
+  Tarjan(ola);
+  printSccGraph(reduceGraph(ola, translation), n_scc);
+  /*DFS(ola);
+  freeGraph(ola);
+  free(color+1);
+  free(pai+1);
+  free(f_time+1);
+  free(d_time+1);*/
+  return 0;
+}
