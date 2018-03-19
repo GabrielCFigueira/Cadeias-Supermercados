0235e1781d2d52357df6858dc90584d4116e7118
RafaelPAndrade <rafael@localhost.here>
2018-03-19 12:26:04 +0000

    Add special sort to fast_list.c
    + Refactor printSccGraph to be more efficient
    
    Basically, a counting insert and a quicksort between connections
    that have the same departure vertex
    
    Changes to be committed:
    	modified:   src/fast_list.c

--- src/fast_list.c
+++ src/fast_list.c
@@ -16,19 +16,49 @@ struct graph {
   int *offset;
 } ;
 
-static int compareCon(const void *a, const void *b);
-static void printCon(Graph g, int u, int v);
+static int compare2Con(const void *a, const void *b);
+static int (*graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertex))[2];
 
-int compareCon(const void *a, const void *b) {
+int (*graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertexes))[2] {
+
+  int i, pos, u;
+  int (*newAdj)[2] = malloc(n_conns*sizeof(*newAdj));
+  /*This is an offset list with an extra position (+2 vs +1) so that
+   * we can have a -1 position of 0, because, during the positioning,
+   * the offsets will shift to the left, and so we will use that extra space
+   * to go back to a normal offset list.*/
+  int * aux_count = (int*) malloc((n_vertexes+2)*sizeof(int));
+  aux_count++;
+  memcpy(aux_count, count, (n_vertexes+1)*sizeof(int));
+
+  for(i=1; i<=n_vertexes; ++i) {
+    aux_count[i]+=aux_count[i-1];
+  }
+
+  for(i=0; i<n_conns; ++i) {
+    u=adjList[i][0];
+    pos=aux_count[u-1]++;
+    newAdj[pos][0] = u;
+    newAdj[pos][1] = adjList[i][1];
+  }
+  aux_count--;
+
+  for(i=1; i<=n_vertexes; ++i) {
+    if(count[i]) {
+      qsort(newAdj+aux_count[i-1],count[i],2*sizeof(int),compare2Con);
+    }
+  }
+  free(adjList);
+  free(aux_count);
+  return newAdj;
+}
+
+
+
+int compare2Con(const void *a, const void *b) {
   int *first_edge = (int*) a;
   int *sec_edge = (int*) b;
-  if (first_edge[0] < sec_edge[0]) {
-    return -1;
-  }
-  if(first_edge[0] > sec_edge[0]) {
-    return 1;
-  }
-  if (first_edge[1] < sec_edge[1]) {
+ if (first_edge[1] < sec_edge[1]) {
     return -1;
   }
   if(first_edge[1] > sec_edge[1]) {
@@ -57,8 +87,7 @@ Graph buildGraph() {
     res->offset[u]++;
   }
 
-  /*Do a couting sort (implement it)*/
-  qsort(res->adjList, res->n_connections, 2*sizeof(int), compareCon);
+  res->adjList = graphSort(res->adjList, res->offset, res->n_connections, res->n_vertexes);
   for(i=1; i<=V; ++i) {
     res->offset[i]+=res->offset[i-1];
   }
@@ -123,14 +152,13 @@ Graph reduceGraph(Graph g, int * translation) {
     }
   }
 
-  /* Do a couting sort (implement it) */
-  qsort(g->adjList, j, 2*sizeof(int), compareCon);
+  g->adjList = graphSort(g->adjList, res->offset, j, res->n_vertexes);
 
   old_u = old_v = 0;
   for(i=0; i < j; ++i) {
-    u=g->adjList[i][0];
-    v=g->adjList[i][1];
-    if(v!=old_v || u!=old_u) {
+    u = g->adjList[i][0];
+    v = g->adjList[i][1];
+    if(old_v != v || old_u != u) {
       res->adjList[n_conns][0]=u;
       res->adjList[n_conns][1]=v;
       old_u=u;
@@ -140,7 +168,6 @@ Graph reduceGraph(Graph g, int * translation) {
       res->offset[u]--;
     }
   }
-
 
   res->n_connections=n_conns;
 
@@ -154,13 +181,7 @@ Graph reduceGraph(Graph g, int * translation) {
 void printSccGraph(Graph g, int nScc) {
   int u;
   printf("%d\n%d\n", nScc, nConnection(g));
-  for(u=1; u<=nVertex(g); ++u)
-    doForEachAdjU(g, u, printCon);
-
-}
-
-
-void printCon(Graph g, int u, int v) {
-  (void) g;
-  printf("%d %d\n", u, v);
+  for(u=0; u<nConnection(g); ++u) {
+    printf("%d %d\n", g->adjList[u][0], g->adjList[u][1]);
+  }
 }
