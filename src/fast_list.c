a0adc3696a022a1034c7d1bd2f9fac6f435bd2b7
RafaelPAndrade <rafael@localhost.here>
2018-03-26 19:28:24 +0100

    Backport Radix sort to fast_list.c
    So that both versions of the fast_list are in sync

--- src/fast_list.c
+++ src/fast_list.c
@@ -17,50 +17,52 @@ struct graph {
   int *offset;
 } ;
 
-static int compare2Con(const void *a, const void *b);
-static int (*graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertex))[2];
+static void graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertexes);
 
-int (*graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertexes))[2] {
+/* sorts a 2xn_conns array, with the first line being the most significant
+ * for sorting purposes
+ * this sort used radixSort (lsd), and couting sort to sort each array
+ */
+void graphSort(int (*adjList)[2], int *count, int n_conns, int n_vertexes) {
 
-  int i, pos, u;
+
+  /* offset goes [0, V]*/
+  int * offset = (int*) calloc (1, (n_vertexes + 1)*sizeof(int));
+  /* goes [0, E-1]*/
   int (*newAdj)[2] = malloc(n_conns*sizeof(*newAdj));
-  /*This is an offset list with an extra position (+2 vs +1) so that
-   * we can have a -1 position of 0, because, during the positioning,
-   * the offsets will shift to the left, and so we will use that extra space
-   * to go back to a normal offset list.*/
-  int * aux_count = (int*) malloc((n_vertexes+2)*sizeof(int));
-  aux_count[0]=0;
-  aux_count++;
-  memcpy(aux_count, count, (n_vertexes+1)*sizeof(int));
 
-  for(i=1; i<=n_vertexes; ++i) {
-    aux_count[i]+=aux_count[i-1];
+  int i;
+
+  for(i = 0; i < n_conns; i++)
+    offset[adjList[i][1]]++;
+
+  for(i = 1; i <= n_vertexes; i++)
+    offset[i] += offset[i - 1];
+
+  --offset;
+  for(i = 0; i < n_conns; i++) {
+    newAdj[offset[adjList[i][1]]][1] = adjList[i][1];
+    newAdj[offset[adjList[i][1]]][0] = adjList[i][0];
+    offset[adjList[i][1]]++;
   }
+  ++offset;
 
-  for(i=0; i<n_conns; ++i) {
-    u=adjList[i][0];
-    pos=aux_count[u-1]++;
-    newAdj[pos][0] = u;
-    newAdj[pos][1] = adjList[i][1];
+
+  memcpy(offset, count, (n_vertexes+1)*sizeof(int));
+
+  for(i = 1; i <= n_vertexes; i++)
+    offset[i] += offset[i - 1];
+
+  --offset;
+  for(i = 0; i < n_conns; i++) {
+    adjList[offset[newAdj[i][0]]][0] = newAdj[i][0];
+    adjList[offset[newAdj[i][0]]][1] = newAdj[i][1];
+    offset[newAdj[i][0]]++;
   }
-  aux_count--;
+  ++offset;
 
-  for(i=1; i<=n_vertexes; ++i) {
-    if(count[i] > 1) {
-      qsort(newAdj+aux_count[i-1],count[i],2*sizeof(int),compare2Con);
-    }
-  }
-  free(adjList);
-  free(aux_count);
-  return newAdj;
-}
-
-int compare2Con(const void *a, const void *b) {
-  int *first_edge = (int*) a;
-  int *sec_edge = (int*) b;
-
-  return first_edge[1] - sec_edge[1];
-
+  free(offset);
+  free(newAdj);
 }
 
 
@@ -82,7 +84,7 @@ Graph buildGraph() {
     res->offset[u]++;
   }
 
-  res->adjList = graphSort(res->adjList, res->offset, res->n_connections, res->n_vertexes);
+  graphSort(res->adjList, res->offset, res->n_connections, res->n_vertexes);
   for(i=1; i<=V; ++i) {
     res->offset[i]+=res->offset[i-1];
   }
@@ -147,7 +149,7 @@ Graph reduceGraph(Graph g, int * translation) {
     }
   }
 
-  g->adjList = graphSort(g->adjList, res->offset, j, res->n_vertexes);
+  graphSort(g->adjList, res->offset, j, res->n_vertexes);
 
   old_u = old_v = 0;
   for(i=0; i < j; ++i) {
